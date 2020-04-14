52b22e72b48fd4fd700beffc6e3facbade27af65
Igor Trochimiuk <3491298@gmail.com>
2020-04-14 09:01:40 +0200

    Zadanie 3.1.1 po zmianie

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        Wardrobe wardrobe = new Wardrobe();
-        wardrobe.height = 210;
-        wardrobe.doorsNumber = 2;
-        wardrobe.colour = "Black";
+        Wardrobe wardrobe = new Wardrobe("Black", 2,210);
         System.out.println("Szafa 1");
         System.out.println("Liczba drzwi :" + wardrobe.doorsNumber);
         System.out.println("Kolor :" + wardrobe.colour);
         System.out.println("Wysokość :" + wardrobe.height);
-        Wardrobe wardrobe2 = new Wardrobe();
-        wardrobe2.height = 150;
-        wardrobe2.doorsNumber = 1;
-        wardrobe2.colour = "Brown";
+        Wardrobe wardrobe2 = new Wardrobe("Brown", 1,150);
         System.out.println("Szafa 2");
         System.out.println("Liczba drzwi :" + wardrobe2.doorsNumber);
         System.out.println("Kolor :" + wardrobe2.colour);
--- src/Wardrobe.java
+++ src/Wardrobe.java
@@ -2,4 +2,9 @@ public class Wardrobe {
     String colour;
     int doorsNumber;
     double height;
+    Wardrobe(String col, int num, double hei){
+        colour = col;
+        doorsNumber = num;
+        height = hei;
+    }
 }
